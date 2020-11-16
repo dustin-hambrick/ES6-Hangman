71117bdaf74aabc77fb1b9a304e4cf3254e7b6c2
Dustin Hambrick <66838808+dustin-hambrick@users.noreply.github.com>
2020-11-16 10:30:14 -0700

    Initial working version with minimal styling.

---
+++ app.js
@@ -0,0 +1,26 @@
+// DOM element selectors
+const messageEl = document.querySelector('#message')
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+const guessedEl = document.querySelector('#guessed')
+
+// select random word from words array
+const randomWord = words[Math.floor(Math.random()*words.length)]
+const guessAmount = randomWord.length
+
+// create new hangman game
+const game1 = new Hangman (randomWord, 7)
+
+// initialize text content for DOM elements
+puzzleEl.textContent = game1.getPuzzle()
+guessesEl.textContent = game1.getStatusMessage()
+guessedEl.textContent = `Guessed Letters: ${game1.guessedLetters.join('')}`
+
+// listen fr guess keypress and render DOM element text content
+window.addEventListener('keypress', function (e) {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.getPuzzle()
+    guessesEl.textContent = game1.getStatusMessage()
+    guessedEl.textContent = `Guessed Letters: ${game1.guessedLetters.join('')}`
+})
